401eb75bd0cb3b4d272f4d7b202092b4206b5f96
Kalel Teixeira <kaleldiasteixeira@gmail.com>
2025-01-20 10:08:31 -0700

    Definida a tela inicial do sistema

---
+++ LeiloesTDSat/src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main (string[] args) {
+        cadastroVIEW tela = new cadastroVIEW ();
+        tela.setVisible(true);
+    }
+
+    private Principal() {
+    }
+
+ 
